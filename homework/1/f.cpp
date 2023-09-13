919caac145e2d37aa2a14f59882531018ad9e19e
wrr606 <wrr606@gmail.com>
2023-09-13 12:01:10 +0800

    re

--- homework/1/f.cpp
+++ homework/1/f.cpp
@@ -4,10 +4,12 @@ using namespace std;
 #define endl '\n'
 typedef long long int ll;
 
+#define N 50
+
 int main(){
-    ll dp[51]={};
+    ll dp[N]={};
     dp[0]=0,dp[1]=1;
-    for(int i=2;i<=50;i++)
+    for(int i=2;i<N;i++)
         dp[i]=dp[i-1]+dp[i-2];
     int n;
     while(cin>>n)
