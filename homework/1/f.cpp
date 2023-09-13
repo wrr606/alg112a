c82b6be8a3a7a9267da72d616d9ca23de0203b0a
wrr606 <wrr606@gmail.com>
2023-09-13 11:30:49 +0800

    re

---
+++ homework/1/f.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+typedef long long int ll;
+
+int main(){
+    ll f[100]={};
+    f[0]=0,f[1]=1;
+    for(int i=2;i<=50;i++)
+        f[i]=f[i-1]+f[i-2];
+    int n;
+    while(cin>>n)
+        cout<<f[n]<<endl;
+}
