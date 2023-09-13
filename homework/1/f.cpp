0a013c46f5a156a2fd21199472b3a0ac4680821c
wrr606 <wrr606@gmail.com>
2023-09-13 11:36:19 +0800

    re

--- homework/1/f.cpp
+++ homework/1/f.cpp
@@ -5,11 +5,11 @@ using namespace std;
 typedef long long int ll;
 
 int main(){
-    ll f[51]={};
-    f[0]=0,f[1]=1;
+    ll dp[51]={};
+    dp[0]=0,dp[1]=1;
     for(int i=2;i<=50;i++)
-        f[i]=f[i-1]+f[i-2];
+        dp[i]=dp[i-1]+dp[i-2];
     int n;
     while(cin>>n)
-        cout<<f[n]<<endl;
+        cout<<dp[n]<<endl;
 }
