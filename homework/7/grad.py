2966c7cdd56f9cac590df3446a899af295da032a
wrr606 <wrr606@gmail.com>
2023-10-25 21:07:35 +0800

    re

---
+++ homework/7/grad.py
@@ -0,0 +1,18 @@
+from engine import Value
+
+a=Value(9)
+b=Value(8)
+c=Value(7)
+
+step=0.01
+
+for i in range(100):
+    f=a**2+b**2+c**2
+    last=f
+    #print(f.data)
+    f.backward()
+    a.data-=a.grad*step
+    b.data-=b.grad*step
+    c.data-=c.grad*step
+
+print("Final values: a =", a.data, ", b =", b.data, ", c =", c.data)
