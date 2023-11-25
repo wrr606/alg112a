3d6c5a367e623f1d33da1b2a64464c4ed12f8a8c
wrr606 <wrr606@gmail.com>
2023-11-25 20:10:35 +0800

    2023/11/25

--- homework/9/h.py
+++ homework/9/h.py
@@ -1,23 +1,27 @@
 import random
-def hillClimbing(f, p, h=1):
-    failCount = 0                    # 失敗次數歸零
-    while (failCount < 10000):       # 如果失敗次數小於一萬次就繼續執行
-        fnow = f(p)                  # fxy 為目前高度
+
+def hillClimbing(f, p, h=0.01):
+    failCount = 0
+    while failCount < 10000:
+        fnow = f(p)
         fneighbor = p.copy()
         for i in range(len(fneighbor)):
-            fneighbor[i]+=random.choice([-h, h, 0])
-        fneighbornow=f(fneighbor)
-        
-        if fneighbornow < fnow:        # 如果移動後高度比現在高
-            p = fneighbor.copy()        #   就移過去
+            fneighbor[i] += random.choice([-h, h, 0])
+        fneighbornow = f(fneighbor)
+
+        if fneighbornow < fnow:
+            p = fneighbor.copy()
             print('p=', p, 'f(p)=', f(p))
-            failCount = 0            # 失敗次數歸零
-        else:                        # 若沒有更高
-            failCount = failCount + 1#   那就又失敗一次
-    return (p,fnow)                 # 結束傳回 （已經失敗超過一萬次了）
+            failCount = 0
+        else:
+            failCount = failCount + 1
+
+        if fnow < 0.001:
+            break
+
+    return p, fnow
 
 def f1(p):
-    return (p[0]**5+1)
+    return p[0] ** 5 + 1
 
 hillClimbing(f1, [0])
-print()
